c88008d7b3a2536bdb5c56ca244ab32a50c2d396
Frank <fv42wid@yahoo.com>
2014-06-08 15:58:20 -0400

    updated initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module Politi
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initalize_on_precompile = false
   end
 end
