7c383b9286105d7e0b9a5e2b57c2bacd7e985cf4
Frank <fv42wid@yahoo.com>
2014-06-08 12:18:11 -0400

    confirmation now sends from info@politicly.org

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,14 @@ Politi::Application.initialize!
 Politi::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
+      address: "smtp.zoho.com",
+      port: 465,
       domain: "politicly.org",
       authentication: "plain",
-      user_name: "fevinci1",
+      user_name: "info@politicly.org",
       password: "imsMay33",
-      enable_starttls_auto: true
+      enable_starttls_auto: true,
+      ssl: true,
+      tls: true
   }
 end
